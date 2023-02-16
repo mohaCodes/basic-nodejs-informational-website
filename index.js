d5a4d30db10d2373bafd7f3e983f6bd796802463
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-16 12:39:19 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send("This is home page")
+    res.status(200).sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.get('/about', (req, res) => {
-    res.status(200).send("This is about page")
+    res.status(200).sendFile(path.join(__dirname, '/about.html'));
 })
 
 app.get('/contact-me', (req, res) => {
-    res.status(200).send("This is contact page")
+    res.status(200).sendFile(path.join(__dirname, '/contact-me.html'));
 })
+
+
+// handles my 404 code
+app.all('*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/404.html'))
+})
+
 
 app.listen(8080, () => {
     console.log("Server listening on 8080...")
